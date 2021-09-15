c76bc44ffa62814cd2a2538169b248dbbc76520e
leanrobert <53313456+leanrobert@users.noreply.github.com>
2021-09-15 19:42:43 -0300

    finalizada v1.0

---
+++ README.md
@@ -0,0 +1,74 @@
+# Proyecto de Quotas
+
+Poryecto dedicado al control de ancho de quota de ancho de banda para clientes por medio del manejo de iptables
+
+
+
+### Manual de instalacion
+Son necesarias las siguientes versiones instaladas:
+Python 3.8.10
+Flask 2.0.1
+Werkzeug 2.0.1
+
+1. Instalar dependencias
+    
+    apt-get install python3-pip apache2 libapache2-mod-wsgi-py3
+    pip3 install Flask
+    pip3 install ipaddress
+
+2. Descargar la aplicacion de NAS Server
+
+    scp -r root@172.27.1.10:/mnt/pruebaRaid/quota /var/www/quota
+    cd /var/www/quota
+    nano quota.wsgi
+
+3. Dentro del archivo wsgi escribimos lo siguiente
+
+    import sys
+    sys.path.insert(0, "/var/www/quota")
+    from app import app as application
+
+4. Editamos sitios disponibles de apache
+
+    cd /etc/apache2/sites-available/
+    nano quota.conf
+
+5. Dentro de quota.conf colocar lo siguiente
+
+    <VirtualHost *>
+        WSGIScriptAlias / /var/www/quota/quota.wsgi
+        WSGIDaemonProcess quota
+        <Directory /var/www/quota>
+        WSGIProcessGroup quota
+        WSGIApplicationGroup %{GLOBAL}
+            Order deny,allow
+            Allow from all
+        </Directory>
+    </VirtualHost>
+
+6. Ejecutamos los siguientes 3 comandos
+
+    a2dissite 000-default.conf
+    a2ensite quota.conf
+    service apache2 reload
+
+7. Permisos
+
+Es necesario permitir correr el comando como sudo sin contraseña, para esto podemos editar ejecutar lo siguiente
+
+    visudo
+
+Dentro del fichero ubicaremos la linea
+
+    %sudo   ALL=(ALL:ALL) ALL
+
+Y la modificamos por
+
+    %sudo   ALL=(ALL:ALL) NOPASSWD:ALL
+
+7. Si todo sale bien el endpoint deberia ser alcanzable el servicio esta en funcionamiento, para debuggear podemos corroboar en el fichero de logs de apache
+
+    tail -f /var/log/apache2/error.log
+
+
+### Endpoints de la app
--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import re, os, ipaddress, subprocess
-from typing import NewType
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.route('/quota/<clientIp>')
 def get_quota_by_ip(clientIp):
-    os.system("sudo /sbin/iptables-save -t mangle -c -f counters.txt")
+    os.system("/sbin/iptables-save -t mangle -c -f counters.txt")
     clientIp32 = clientIp + "/32"
     with open("counters.txt", 'r') as read_obj:
         for line in read_obj:
@@ -17,10 +16,10 @@ def get_quota_by_ip(clientIp):
 
 @app.route('/resetQuota')
 def reset_quota():
-    os.system('sudo /sbin/iptables -t mangle -F')
-    os.system('sudo /sbin/iptables -t mangle -X')
-    os.system('sudo /sbin/iptables-save -t mangle -f /home/sistemas/lrobert/endpoint/quota.txt')
-    os.system('sudo /sbin/iptables-save -t mangle -f /home/sistemas/lrobert/endpoint/counters.txt')
+    os.system('/sbin/iptables -t mangle -F')
+    os.system('/sbin/iptables -t mangle -X')
+    os.system('/sbin/iptables-save -t mangle -f quota.txt')
+    os.system('/sbin/iptables-save -t mangle -f counters.txt')
     return(jsonify({ 'message': 'Ips reseteadas con exito'}))
 
 @app.route('/generate', methods=['POST'])
@@ -52,12 +51,12 @@ def create_node_rules():
             networklist = list(network.subnets())
             
             netupper = networklist[0]
-            markRules.append(f':{str(netupper)} - [0:0]\n')
-            jumpRules.append(f'-A {str(network)} -s {str(netupper)} -j {str(netupper)}\n')
+            markRules.append(":" + str(netupper) + " - [0:0]\n")
+            jumpRules.append("-A {str(network)} -s {str(netupper)} -j {str(netupper)}\n")
 
             netlower = networklist[1]
-            markRules.append(f':{str(netlower)} - [0:0]\n')
-            jumpRules.append(f'-A {str(network)} -s {str(netlower)} -j {str(netlower)}\n')
+            markRules.append(":" + str(netlower) + " - [0:0]\n")
+            jumpRules.append("-A " + str(network) + " -s " + str(netlower) + " -j " + str(netlower) + "\n")
 
             escribirReglas(ipsSi, netlower)
             escribirReglas(ipsSi, netupper)
@@ -66,7 +65,7 @@ def create_node_rules():
                 return
             else:
                 for ip in ipsSi:
-                    clientRules.append(f'-A {str(network)} -s {str(ip)}/32 -j ACCEPT\n')
+                    clientRules.append("-A " + str(network) + " -s " + str(ip) + "/32 -j ACCEPT\n")
                 return
 
     markRules = []
@@ -76,9 +75,9 @@ def create_node_rules():
 
     nodos = request.get_json()
     for subnet in nodos:
-        network = ipaddress.IPv4Network(f'10.{str(subnet)}.0.0/16')
-        jumpGeneralRule.append(f'-A PREROUTING -j {network}\n')
-        markRules.append(f':{str(network)} - [0:0]\n')
+        network = ipaddress.IPv4Network("10." + str(subnet) + ".0.0/16")
+        jumpGeneralRule.append("-A PREROUTING -j " + network + "\n")
+        markRules.append(":" + str(network) + " - [0:0]\n")
         escribirReglas(nodos[subnet], network)   
 
     generarArchivos()
@@ -86,7 +85,7 @@ def create_node_rules():
 
 @app.route('/apply', methods=['POST'])
 def apply_iptables_rules():
-    cmd = ["sudo", "/sbin/iptables-restore", "-T", "mangle", "quota.txt"]
+    cmd = ["/sbin/iptables-restore", "-T", "mangle", "quota.txt"]
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
     process.communicate()
     return jsonify({ 'message': 'iptables executed'})
@@ -95,10 +94,107 @@ def apply_iptables_rules():
 def test():
     nodos = request.get_json()
     for subnet in nodos:
-        print(f'la red es {subnet} y tiene las ips {nodos[subnet]}')
-    return(f'<h1>{nodos}</h1>')
+        print("la red es " + str(subnet) + " y tiene las ips " + str(nodos[subnet]))
+    return("<h1>" + str(nodos) + "</h1>")
 
 
-'''Hasta el momento el problema esta con la creacion de los archivos y permisos, el siguiente paso
-seria poder colocar todo bajo el mismo usuario, para ser propietario y de ahi generar el archivo
-solo, ademas faltaria documentar el server completo y finalmente hacer las pruebas'''
+
+import re, os, ipaddress, subprocess
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/quota/<clientIp>')
+def get_quota_by_ip(clientIp):
+    os.system("/sbin/iptables-save -t mangle -c -f /var/www/quota/counters.txt")
+    clientIp32 = clientIp + "/32"
+    with open("/var/www/quota/counters.txt", 'r') as read_obj:
+        for line in read_obj:
+            if clientIp32 in line:
+                match = re.findall("\d*]", line)
+                client = { "ip": clientIp, "qdownload": match[0][:-1]}
+                return jsonify(client)
+
+@app.route('/resetQuota', methods=['POST'])
+def reset_quota():
+    os.system('sudo /sbin/iptables -t mangle -F')
+    os.system('sudo /sbin/iptables -t mangle -X')
+    os.system('sudo /sbin/iptables-save -t mangle -f /var/www/quota/quota.txt')
+    os.system('sudo chmod 666 /var/www/quota/quota.txt')
+    os.system('sudo /sbin/iptables-save -t mangle -f /var/www/quota/counters.txt')
+    os.system('sudo chmod 666 /var/www/quota/counters.txt')
+    return(jsonify({ 'message': 'Ips reseteadas con exito'}))
+
+@app.route('/generate', methods=['POST'])
+def create_node_rules():
+    def generarArchivos():
+        readfile = open("/var/www/quota/quota.txt", "r")
+        readed = readfile.readlines()
+        header = readed[0:7]
+        footer = readed[-2:]
+        readfile.close()
+
+        file = open("/var/www/quota/quota.txt", "w")
+        file.writelines(header)
+        file.writelines(markRules)
+        file.writelines(jumpGeneralRule)
+        file.writelines(jumpRules)
+        file.writelines(clientRules)
+        file.writelines(footer)
+
+    def escribirReglas(ips, network):
+        ipsSi = []
+
+        for ip in ips:
+            ip = ipaddress.IPv4Address(ip)
+            if ip in network:
+                ipsSi.append(ip)
+
+        if len(ipsSi) > 4:
+            networklist = list(network.subnets())
+
+            netupper = networklist[0]
+            markRules.append(":" + str(netupper) + " - [0:0]\n")
+            jumpRules.append("-A " + str(network) + " -s " + str(netupper) + " -j " + str(netupper) + "\n")
+            netlower = networklist[1]
+            markRules.append(":" + str(netlower) + " - [0:0]\n")
+            jumpRules.append("-A " + str(network) + " -s " + str(netlower) + " -j " + str(netlower) + "\n")
+
+            escribirReglas(ipsSi, netlower)
+            escribirReglas(ipsSi, netupper)
+        else:
+            if len(ipsSi) == 0:
+                return
+            else:
+                for ip in ipsSi:
+                    clientRules.append("-A " + str(network) + " -s " + str(ip) + "/32 -j ACCEPT\n")
+                return
+
+    markRules = []
+    jumpRules = []
+    clientRules = []
+    jumpGeneralRule = []
+
+    nodos = request.get_json()
+    for subnet in nodos:
+        network = ipaddress.IPv4Network("10." + str(subnet) + ".0.0/16")
+        jumpGeneralRule.append("-A PREROUTING -j " + str(network) + "\n")
+        markRules.append(":" + str(network) + " - [0:0]\n")
+        escribirReglas(nodos[subnet], network)
+
+    generarArchivos()
+    return(jsonify({ 'message': 'reglas de mangle creadas con exito'}))
+
+@app.route('/apply', methods=['POST'])
+def apply_iptables_rules():
+    cmd = ["sudo", "/sbin/iptables-restore", "-T", "mangle", "/var/www/quota/quota.txt"]
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    process.communicate()
+    return jsonify({ 'message': 'iptables executed'})
+
+@app.route('/test', methods=['GET', 'POST'])
+def test():
+    nodos = request.get_json()
+    for subnet in nodos:
+        print("la red es " + str(subnet) + " y tiene las ips " + str(nodos[subnet]))
+    return("<h1>" + str(nodos) + "</h1>")
